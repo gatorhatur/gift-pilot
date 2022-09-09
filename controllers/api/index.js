34b4753060f537fd72f8054d4e0e81917934965e
Brandon Davis <brancdavis@gmail.com>
2022-09-08 20:46:24 -0400

    initial creation of routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,9 @@ const router = require('express').Router();
 // const userRoutes = require('./user-routes.js');
 // const postRoutes = require('./post-routes');
 // const commentRoutes = require('./comment-routes');
+const listItemRoutes = require('./list-item-routes');
 
-// router.use('/users', userRoutes);
+router.use('/listItems', listItemRoutes);
 // router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
 
