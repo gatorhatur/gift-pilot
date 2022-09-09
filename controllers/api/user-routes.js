13878f6f2d1f9af9bf3a102f8f259a30e0e73598
Max Taylor <maxbransontaylor@gmail.com>
2022-09-09 17:20:13 -0400

    fixing user routes and adding comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { User, ListItem, Purchase, FriendList } = require("../../models");
-
+//get all users, can add parameters to the req body
 router.get("/", (req, res) => {
   User.findAll({
-    attributes: {exclude: ["password"]}
+    attributes: { exclude: ["password"] },
   })
     .then((dbUserData) => res.status(200).json(dbUserData))
     .catch((err) => {
@@ -11,53 +11,25 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
-router.get("/:id", (req, res) => {
-  User.findOne({
-    attributes: { exclude: ["password"] },
-    where: { id: req.params.id },
-    include: [{ model: ListItem, include: [{ model: Purchase }] }],
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "could not find user with this id" });
-        return;
-      }
-      res.status(200).json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+//show users own gift list without including purchase data
 router.get("/dashboard", (req, res) => {
   User.findOne({
     where: { id: req.session.user_id },
     include: [{ model: ListItem }],
-  });
-});
-router.get("/:id", (req, res) => {
-  User.findOne({
-    attributes: { exclude: ["password"] },
-    where: { id: req.params.id },
-    include: [{ model: ListItem }],
   })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "could not find user with this id" });
-        return;
-      }
-      res.status(200).json(dbUserData);
-    })
+    .then((data) => res.status(200).json(data))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+//create a new user/ sign up
 router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
+    birthDate: req.body.birthDate,
   })
     .then((dbUserData) => {
       req.session.save(() => {
@@ -105,6 +77,7 @@ router.post("/logout", (req, res) => {
     res.status(404).end();
   }
 });
+//get a list of all users friends
 router.get("/friends", (req, res) => {
   FriendList.findAll({
     where: { user_id: req.session.user_id },
@@ -118,7 +91,7 @@ router.get("/friends", (req, res) => {
       res.status(400).json(err);
     });
 });
-
+//make a new friend pairing given user id of friend as param
 router.post("/friends/:id", (req, res) => {
   FriendList.bulkCreate(
     { user_id: req.session.user_id, friend_id: req.params.id },
@@ -130,5 +103,23 @@ router.post("/friends/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
-
+//use this route for viewing lists that do NOT belong to the user
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: { id: req.params.id },
+    include: [{ model: ListItem, include: [{ model: Purchase }] }],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "could not find user with this id" });
+        return;
+      }
+      res.status(200).json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 module.exports = router;
